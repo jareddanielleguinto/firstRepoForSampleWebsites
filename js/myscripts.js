a6c6d64a29797ac03d2ce25d17ca317b017a1c60
Jared Guinto <jaredstifflerdanielle@gmail.com>
2019-01-11 15:33:34 +0800

    Initial commit

---
+++ js/myscripts.js
@@ -0,0 +1,97 @@
+window.sr = ScrollReveal();
+sr.reveal('.home-section-2-upperLeft', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '50px',
+    viewFactor: 0.2
+});
+sr.reveal('.home-section-2-upperRight', {
+    duration: 3000,
+    origin: 'left',
+    distance: '20px',
+    viewFactor: 0.2
+});
+sr.reveal('.home-section-2-lowerRight', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '50px',
+    viewFactor: 0.2
+});
+sr.reveal('.lowerLeft-image1', {
+    duration: 3000,
+    origin: 'left',
+    distance: '50px',
+    viewFactor: 0.2
+});
+sr.reveal('.lowerLeft-image2', {
+    duration: 4000,
+    origin: 'left',
+    distance: '50px',
+    viewFactor: 0.2
+});
+sr.reveal('.lowerLeft-image3', {
+    duration: 5000,
+    origin: 'left',
+    distance: '50px',
+    viewFactor: 0.2
+});
+sr.reveal('.newsFadeicon', {
+    duration: 2000,
+    origin: 'right',
+    distance: '10px',
+    viewFactor: 0.2
+});
+sr.reveal('.newsFadetext', {
+    duration: 3000,
+    origin: 'right',
+    distance: '10px',
+    viewFactor: 0.2
+});
+sr.reveal('.leftPart-paragraph-1', {
+    duration: 4000,
+    origin: 'right',
+    distance: '10px',
+    viewFactor: 0.2
+});
+sr.reveal('.leftPart-paragraph-2', {
+    duration: 4500,
+    origin: 'right',
+    distance: '10px',
+    viewFactor: 0.2
+});
+sr.reveal('.dottedbox-1-text1', {
+    duration: 4000,
+    origin: 'left',
+    distance: '10px',
+    viewFactor: 0.2
+});
+sr.reveal('.dottedbox-1-text2', {
+    duration: 4000,
+    origin: 'right',
+    distance: '10px',
+    viewFactor: 0.2
+});
+sr.reveal('.contactFadeIn', {
+    duration: 2000,
+    origin: 'top',
+    distance: '10px',
+    viewFactor: 0.2
+});
+sr.reveal('.name', {
+    duration: 2000,
+    origin: 'left',
+    distance: '10px',
+    viewFactor: 0.2
+});
+sr.reveal('.email', {
+    duration: 2000,
+    origin: 'right',
+    distance: '10px',
+    viewFactor: 0.2
+});
+sr.reveal('.message', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '10px',
+    viewFactor: 0.2
+});
